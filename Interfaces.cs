c079a5d7291d86befbadba5074d6a104a61c63cf
Breeze16 <37762021+Breeze16@users.noreply.github.com>
2018-04-24 18:53:38 +0800

    Create Interfaces.cs

---
+++ Interfaces.cs
@@ -0,0 +1,42 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUserInterface
+{
+	void ChangeActionManager ();
+
+	void GameOver ();
+}
+
+public interface ISceneController
+{
+	void Restart ();
+
+	void StartGame ();
+
+	void Pause ();
+
+	void Resume ();
+}
+
+public interface IGamePlaying
+{
+	void ShootDisk ();
+
+	void FreeDisk ();
+
+	void NextRound ();
+
+	void addScoreByRound ();
+
+	float getSpeedByRound ();
+}
+
+public interface IActionManager
+{
+	void playDisk ();
+
+	void clearActions ();
+}
